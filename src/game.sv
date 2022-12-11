e959491d8b4ce171623a7d117e2bf6f8cc414904
KMitzel <kdmitzel43@yahoo.com>
2022-12-11 13:39:24 -0500

    game logic updated to frame

--- src/game.sv
+++ src/game.sv
@@ -7,13 +7,16 @@
 module game (
     input wire clk,
     input wire [15:0] sw,
-    input wire btnc,
+    input wire rst,
     input wire btnu,
-    input wire receive_axiov,
+    input wire [10:0] hcount, 
+    input wire [9:0] vcount,
+    input wire receive_axiiv,
     input wire [10:0] r_opp_x,
     input wire [10:0] r_opp_y,
     input wire [8:0] r_opp_dir,
     input wire [2:0] r_opp_game,
+    input wire r_opp_rst,
 
     output logic [10:0] player_x,
     output logic [10:0] player_y,
@@ -48,74 +51,72 @@ logic [3:0] speed;
 assign speed = 6;
 
 xilinx_single_port_ram_read_first #(
-        .RAM_WIDTH(11),
-        .RAM_DEPTH(360),
-        .RAM_PERFORMANCE("HIGH_PERFORMANCE"),
-        .INIT_FILE(`FPATH(cos.mem))                    
+    .RAM_WIDTH(11),
+    .RAM_DEPTH(360),
+    .RAM_PERFORMANCE("HIGH_PERFORMANCE"),
+    .INIT_FILE(`FPATH(cos.mem))                    
 ) p_cos (
-        .addra(player_dir),
-        .dina(11'b0),       
-        .clka(clk),
-        .wea(1'b0),
-        .ena(1'b1),
-        .rsta(btnc),
-        .regcea(1'b1),
-        .douta(p_c)
-    );
+    .addra(player_dir),
+    .dina(11'b0),       
+    .clka(clk),
+    .wea(1'b0),
+    .ena(1'b1),
+    .rsta(rst),
+    .regcea(1'b1),
+    .douta(p_c)
+);
 
-    xilinx_single_port_ram_read_first #(
-        .RAM_WIDTH(11),
-        .RAM_DEPTH(360),
-        .RAM_PERFORMANCE("HIGH_PERFORMANCE"),
-        .INIT_FILE(`FPATH(sin.mem))                    
-    ) p_sin (
-        .addra(player_dir),
-        .dina(11'b0),       
-        .clka(clk),
-        .wea(1'b0),
-        .ena(1'b1),
-        .rsta(btnc),
-        .regcea(1'b1),
-        .douta(p_s)
-    );
+xilinx_single_port_ram_read_first #(
+    .RAM_WIDTH(11),
+    .RAM_DEPTH(360),
+    .RAM_PERFORMANCE("HIGH_PERFORMANCE"),
+    .INIT_FILE(`FPATH(sin.mem))                    
+) p_sin (
+    .addra(player_dir),
+    .dina(11'b0),       
+    .clka(clk),
+    .wea(1'b0),
+    .ena(1'b1),
+    .rsta(rst),
+    .regcea(1'b1),
+    .douta(p_s)
+);
 
-    xilinx_single_port_ram_read_first #(
-        .RAM_WIDTH(11),
-        .RAM_DEPTH(360),
-        .RAM_PERFORMANCE("HIGH_PERFORMANCE"),
-        .INIT_FILE(`FPATH(cos.mem))                    
-    ) o_cos (
-        .addra(r_opp_dir),
-        .dina(11'b0),       
-        .clka(clk),
-        .wea(1'b0),
-        .ena(1'b1),
-        .rsta(btnc),
-        .regcea(1'b1),
-        .douta(o_c)
-    );
+xilinx_single_port_ram_read_first #(
+    .RAM_WIDTH(11),
+    .RAM_DEPTH(360),
+    .RAM_PERFORMANCE("HIGH_PERFORMANCE"),
+    .INIT_FILE(`FPATH(cos.mem))                    
+) o_cos (
+    .addra(r_opp_dir),
+    .dina(11'b0),       
+    .clka(clk),
+    .wea(1'b0),
+    .ena(1'b1),
+    .rsta(rst),
+    .regcea(1'b1),
+    .douta(o_c)
+);
 
-    xilinx_single_port_ram_read_first #(
-        .RAM_WIDTH(11),
-        .RAM_DEPTH(360),
-        .RAM_PERFORMANCE("HIGH_PERFORMANCE"),
-        .INIT_FILE(`FPATH(sin.mem))                    
-    ) o_sin (
-        .addra(r_opp_dir),
-        .dina(11'b0),       
-        .clka(clk),
-        .wea(1'b0),
-        .ena(1'b1),
-        .rsta(btnc),
-        .regcea(1'b1),
-        .douta(o_s)
-    );
+xilinx_single_port_ram_read_first #(
+    .RAM_WIDTH(11),
+    .RAM_DEPTH(360),
+    .RAM_PERFORMANCE("HIGH_PERFORMANCE"),
+    .INIT_FILE(`FPATH(sin.mem))                    
+) o_sin (
+    .addra(r_opp_dir),
+    .dina(11'b0),       
+    .clka(clk),
+    .wea(1'b0),
+    .ena(1'b1),
+    .rsta(rst),
+    .regcea(1'b1),
+    .douta(o_s)
+);
 
 always_ff @(posedge clk) begin
-    if (btnc) begin
-        wait_state <= 0;
-        reset_state <= 0;
-        game_state <= 1;
+    if (rst || r_opp_rst) begin
+        game_state <= 0;
         game_status <= 0;
         laps <= 0;
         
@@ -124,88 +125,108 @@ always_ff @(posedge clk) begin
         player_y <= 100;
         opponent_x <= 300;
         opponent_y <= 100;
-        player_dir <= 90;
+        player_direction <= 90;
         opp_dir <= 90;
         i_player_x <= 0;
         i_player_y <= 0;
         i_opp_x <= 0;
         i_opp_y <= 0;
+    end else if (game_state) begin
+        game_state <= 1;
+        game_status <= 1;
     end else begin
-        case ({wait_state, reset_state, game_state})
-            3'b100: begin
-                i_player_x <= 0;
-                i_player_y <= 0;
-                i_opp_x <= 0;
-                i_opp_y <= 0;
+        if (hcount == 1200 & vcount == 800) begin
+            // loss condition
+            if (r_opp_game == 1) begin
+                game_state <= 1;
+                game_status <= 1;
+            end
 
-                //might be a syncing issue here
-                if (btnu) begin
-                    game_status <= 1;
-                end else begin
-                    game_status <= 0;
+            // Check if completed lap (checks range of position (which will be in track) %% direction (to check they are going right way))
+            if ((player_x == 0 && player_y == 1) && (player_dir >= 0 && player_dir <= 180)) begin
+                laps <= laps + 1;
+            end
+
+            // win condition
+            if (laps == 3) begin
+                game_status <= 1; 
+                game_state <= 1;
+            end
+
+            // Turning Mechanic
+            if ((sw[15] == 1 && sw[0] == 1) || (sw[15] == 0 && sw[0] == 0)) begin
+                player_dir <= player_dir;
+            end else if (sw[15] == 1) begin
+                player_dir <= player_dir + 1;
+                if (player_dir == 359) begin
+                    player_dir <= 0;
                 end
-                if (game_status == 1 & r_opp_game == 1) begin
-                    wait_state <= 0;
-                    game_state <= 1;
+            end else if (sw[0] == 1) begin
+                player_dir <= player_dir -1;
+                if (player_dir == 1) begin
+                    player_dir <= 360;
                 end
             end
-            3'b001: begin
-                // loss condition
-                if (r_opp_game == 2) begin
-                    game_state <= 0;
-                    reset_state <= 1;
-                end
 
-                // Check if completed lap (checks range of position (which will be in track) %% direction (to check they are going right way))
-                if ((player_x == 0 && player_y == 1) && (player_dir >= 0 && player_dir <= 180)) begin
-                    laps <= laps + 1; 
-                end
-
-                // win condition
-                if (laps == 3) begin
-                    game_status <= 2; 
-                    game_state <= 0;
-                    reset_state <= 1;
-                end
-                
-                // Turning Mechanic
-                if ((sw[15] == 1 && sw[0] == 1) || (sw[15] == 0 && sw[0] == 0)) begin
-                    player_dir <= player_dir;
-                end else if (sw[15] == 1) begin
-                    player_dir <= player_dir + 1;
-                    if (player_dir == 359) begin
-                        player_dir <= 0;
-                    end
-                end else if (sw[0] == 1) begin
-                    player_dir <= player_dir -1;
-                    if (player_dir == 1) begin
-                        player_dir <= 360;
-                    end
-                end
-
-                if (receive_axiov) begin
-                    i_player_x <= $signed(speed * p_c);
-                    i_player_y <= $signed(speed * p_s);
-                    i_opp_x <= $signed(speed * o_c);
-                    i_opp_y <= $signed(speed * o_s);
-                end else begin
-                    i_player_x <= 0;
-                    i_player_y <= 0;
-                    i_opp_x <= 0;
-                    i_opp_y <= 0;
-                end
-
+            // Player x
+            if (player_x + i_player_x >= 1024 - 32) begin 
+                player_x <= 1024 - 32;
+            end else if (player_x + i_player_x <= 0 + 32) begin
+                player_x <= 0 + 32;
+            end else begin
                 player_x <= player_x + i_player_x;
+            end
+            // Player y
+            if (player_y + i_player_y >= 768 - 32) begin 
+                player_y <= 768 - 32;
+            end else if (player_y + i_player_y <= 0 + 32) begin
+                player_y <= 0 + 32;
+            end else begin
                 player_y <= player_y + i_player_y;
-                player_direction <= player_dir;
+            end
+
+            // Opponent x
+            if (opponent_x + i_opp_x >= 1024 - 32) begin 
+                opponent_x <= 1024 - 32;
+            end else if (opponent_x + i_opp_x <= 0 + 32) begin
+                opponent_x <= 0 + 32;
+            end else begin
                 opponent_x <= opponent_x + i_opp_x;
-                opponent_y <= opponent_y + i_opp_y; 
             end
-            3'b010: begin
-                //reset everything and switch back to wait state
-                // check to make sure other user has reset too
+
+            // Opponent y
+            if (opponent_y + i_opp_y >= 768 - 32) begin 
+                opponent_y <= 768 - 32;
+            end else if (opponent_y + i_opp_y <= 0 + 32) begin
+                opponent_y <= 0 + 32;
+            end else begin
+                opponent_y <= opponent_y + i_opp_y;
             end
-        endcase
+
+            player_direction <= player_dir;
+        end
+
+        //Collisions
+        // if (receive_axiiv) begin
+        //     // Check collision right side
+        //     if (player_x + i_player_x + 32 >= opponent_x + i_opp_x - 32 && player_x + i_player_x + 32 >= opponent_x + i_opp_x - 32) begin
+            
+        //     end else if (player_x + i_player_x + 32 >= opponent_x + i_opp_x - 32) begin
+
+        //     end else begin
+        //         i_player_x <= $signed(speed * p_c);
+        //         i_player_y <= $signed(-1 * speed * p_s);
+        //         i_opp_x <= $signed(speed * o_c);
+        //         i_opp_y <= $signed(-1 * speed * o_s);
+        //     end
+        // end 
+
+        if (receive_axiiv) begin
+            i_player_x <= $signed(speed * p_c);
+            i_player_y <= $signed(-1 * speed * p_s);
+            i_opp_x <= $signed(speed * o_c);
+            i_opp_y <= $signed(-1 * speed * o_s);
+        end
     end
 end
     
--- src/top_level.sv
+++ src/top_level.sv
@@ -25,7 +25,7 @@ module top_level(
     );
 
     logic sys_rst; //global system reset
-    assign sys_rst = btnc || r_opponent_reset; //just done to make sys_rst more obvious
+    assign sys_rst = btnc; //|| r_opponent_reset; //just done to make sys_rst more obvious
     //assign led = sw; //switches drive LED (change if you want)
 
     //vga module generation signals:
@@ -56,11 +56,11 @@ module top_level(
     logic [8:0] r_opponent_dir; 
     logic [2:0] r_opponent_game; 
     logic r_opponent_reset;
-    assign r_opponent_x = receive_axiod[43:33];
-    assign r_opponent_y = receive_axiod[31:21];
-    assign r_opponent_dir = receive_axiod[19:11];
-    assign r_opponent_game = receive_axiod[7:5];
-    assign r_opponent_reset = receive_axiod[3];
+    assign r_opponent_x = sync_receive[43:33];
+    assign r_opponent_y = sync_receive[31:21];
+    assign r_opponent_dir = sync_receive[19:11];
+    assign r_opponent_game = sync_receive[7:5];
+    assign r_opponent_reset = sync_receive[3];
 
     logic [10:0] player_x;
     logic [10:0] player_y;
@@ -71,10 +71,67 @@ module top_level(
 
     logic clk_65mhz;
 
+    logic [43:0] sync_receive; 
+
     clk_wiz_0_clk_wiz clk_maker(
         .clk_in1(clk_100mhz),
         .eth_clk(eth_refclk),
         .vga_clk(clk_65mhz));
+
+    xilinx_true_dual_port_read_first_2_clock_ram #(
+        .RAM_WIDTH(44),
+        .RAM_DEPTH(2))
+    eth_buffer (
+        //Write Side (50MHz)
+        .addra(0),
+        .clka(eth_refclk), //NEW FOR LAB 04B
+        .wea(1),
+        .dina(receive_axiod),
+        .ena(receive_axiov),
+        .regcea(1'b1),
+        .rsta(sys_rst),
+        .douta(),
+        //Read Side (65 MHz)
+        .addrb(0),
+        .dinb(44'b0),
+        .clkb(clk_65mhz),
+        .web(1'b0),
+        .enb(1'b1),
+        .rstb(sys_rst),
+        .regceb(1'b1),
+        .doutb(sync_receive)
+    );
+
+    logic [20:0] screen_sync; 
+    logic [10:0] sync_hcount; 
+    assign sync_hcount = screen_sync[20:10];
+
+    logic [9:0] sync_vcount; 
+    assign sync_vcount = screen_sync[9:0];
+
+    xilinx_true_dual_port_read_first_2_clock_ram #(
+        .RAM_WIDTH(21),
+        .RAM_DEPTH(2))
+    vga_buffer (
+        //Write Side (.67MHz)
+        .addra(0),
+        .clka(clk_65mhz), //NEW FOR LAB 04B
+        .wea(1),
+        .dina({hcount, vcount}),
+        .ena(1'b1),
+        .regcea(1'b1),
+        .rsta(sys_rst),
+        .douta(),
+        //Read Side (50 MHz)
+        .addrb(0),
+        .dinb(21'b0),
+        .clkb(eth_refclk),
+        .web(1'b0),
+        .enb(1'b1),
+        .rstb(sys_rst),
+        .regceb(1'b1),
+        .doutb(screen_sync)
+    );
     
     vga vga_gen(
         .pixel_clk_in(clk_65mhz),
@@ -89,23 +146,32 @@ module top_level(
         .rst_in(sys_rst),
         .hcount_in(hcount),
         .vcount_in(vcount),
+        .pixel_out(pixel_out_track),
+        // .player_x(11'd191),
+        // .player_y(11'd191),
+        // .opponent_x(11'd319),
+        // .opponent_y(11'd319));
         .player_x(player_x),
         .player_y(player_y),
         .opponent_x(opponent_x),
-        .opponent_y(opponent_y),
-        .pixel_out(pixel_out_track));
+        .opponent_y(opponent_y));
 
     racer_view racer_viewer(
         .clk_in(clk_65mhz),
         .rst_in(sys_rst),
         .hcount_in(hcount),
         .vcount_in(vcount),
+        .pixel_out(pixel_out_racer),
         .player_x(player_x),
         .player_y(player_y),
         .direction(player_dir),
         .opponent_x(opponent_x),
-        .opponent_y(opponent_y),
-        .pixel_out(pixel_out_racer));
+        .opponent_y(opponent_y));
+        // .player_x(11'd191),
+        // .player_y(11'd191),
+        // .direction(270),
+        // .opponent_x(11'd320),
+        // .opponent_y(11'd320));
 
     forward_view forward_viewer(
         .clk_in(clk_65mhz),
@@ -117,10 +183,16 @@ module top_level(
         .direction(player_dir),
         .opponent_x(opponent_x),
         .opponent_y(opponent_y),
+        // .player_x(11'd191),
+        // .player_y(11'd191),
+        // .direction(270),
+        // .opponent_x(11'd320),
+        // .opponent_y(11'd320),
         .pixel_out(pixel_out_forward));
 
     receive r1(.eth_refclk(eth_refclk),
-               .btnc(sys_rst),
+               //.btnc(sys_rst),
+               .btnc(btnc),
                .eth_crsdv(eth_crsdv),
                .eth_rxd(eth_rxd),
                .axiov(receive_axiov),
@@ -128,25 +200,41 @@ module top_level(
                .eth_rstn(eth_rstn));
     
     transmit t1(.eth_clk(eth_refclk),
-                .eth_rst(sys_rst),
-                .hcount(hcount),
-                .vcount(vcount),
+                //.eth_rst(sys_rst),
+                .eth_rst(btnc),
+                // .hcount(hcount),
+                // .vcount(vcount),
+                .hcount(sync_hcount),
+                .vcount(sync_vcount),
                 .player_x(player_x),
                 .player_y(player_x),
                 .direction(player_dir),
                 .game_stat(p_game_stat),
+                // .player_x(11'd191),
+                // .player_y(11'd191),
+                // .direction(270),
+                // .game_stat(1),
                 .eth_txd(eth_txd),
                 .eth_txen(eth_txen));
 
-    game g1(.clk(eth_refclk),
+    game g1(.clk(clk_65mhz),
             .sw(sw),
-            .btnc(sys_rst),
+            .rst(sys_rst),
             .btnu(btnu),
-            .receive_axiov(receive_axiov),
+            .hcount(hcount),
+            .vcount(vcount),
+            .receive_axiiv(receive_axiov),
             .r_opp_x(r_opponent_x),
             .r_opp_y(r_opponent_y),
             .r_opp_dir(r_opponent_dir),
             .r_opp_game(r_opponent_game),
+            .r_opp_rst(r_opponent_reset),
+            // .receive_axiiv(1),
+            // .r_opp_x(100),
+            // .r_opp_y(100),
+            // .r_opp_dir(270),
+            // .r_opp_game(1),
+            // .r_opp_rst(0),
             .player_x(player_x),
             .player_y(player_y),
             .player_direction(player_dir),
@@ -188,8 +276,11 @@ module top_level(
     assign vga_hs = ~hsync_pipe[8];
     assign vga_vs = ~vsync_pipe[8];
 
+    logic [2:0] receive_o_counter;
+
     always_ff @(posedge eth_refclk) begin
-        if (sys_rst) begin
+        //if (sys_rst) begin
+        if (btnc) begin
             led[13:0] <= 0;
             buffer <= 0;
             hcount_f <= 0;
